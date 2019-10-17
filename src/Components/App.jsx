dba682d513d6ec6ca1452dc7ab2b5a9836bc5564
Williams Ohworuka <47520577+WilliamsOhworuka@users.noreply.github.com>
2019-10-17 02:13:37 +0100

    test

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from './GameContainer/Container';
+import Container from './Container/Container';
 import BoardContextProvider from './Context/BoardContext';
 import './app.scss';
 
